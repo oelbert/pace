b8d0b7cb8f07b3585f75c76bca15341e3d3aed55
Oliver Elbert <oliver.elbert36@gmail.com>
2023-04-25 13:36:01 -0400

    adding test for ZeZt

---
+++ physics/tests/savepoint/translate/translate_zezt.py
@@ -0,0 +1,84 @@
+import pace.dsl
+import pace.util
+from pace.dsl.typing import FloatField, FloatFieldIJ
+from pace.physics._config import PhysicsConfig
+from pace.physics.stencils.microphysics_v3.sedimentation import (
+    calc_edge_and_terminal_height,
+)
+from pace.stencils.testing.translate_physics import TranslatePhysicsFortranData2Py
+from pace.util import X_DIM, Y_DIM, Z_INTERFACE_DIM
+
+
+class ZeZt:
+    def __init__(
+        self,
+        stencil_factory: pace.dsl.StencilFactory,
+        config,
+        timestep,
+    ):
+        self.config = config
+        self._calc_edge_and_terminal_height = stencil_factory.from_dims_halo(
+            func=calc_edge_and_terminal_height,
+            externals={"timestep": timestep},
+            compute_dims=[X_DIM, Y_DIM, Z_INTERFACE_DIM],
+        )
+
+    def __call__(
+        self,
+        z_surface: FloatFieldIJ,
+        z_edge: FloatField,
+        z_terminal: FloatField,
+        delz: FloatField,
+        v_terminal: FloatField,
+    ):
+        self._calc_edge_and_terminal_height(
+            z_surface,
+            z_edge,
+            z_terminal,
+            delz,
+            v_terminal,
+        )
+
+
+class TranslateZeZt(TranslatePhysicsFortranData2Py):
+    def __init__(
+        self,
+        grid,
+        namelist: pace.util.Namelist,
+        stencil_factory: pace.dsl.StencilFactory,
+    ):
+        super().__init__(grid, namelist, stencil_factory)
+        self.in_vars["data_vars"] = {
+            "z_surface": {"serialname": "zz_zs", "mp3": True},
+            "z_edge": {"serialname": "zz_ze", "mp3": True},
+            "z_terminal": {"serialname": "zz_zt", "mp3": True},
+            "delz": {"serialname": "zz_dz", "mp3": True},
+            "v_terminal": {"serialname": "zz_vt", "mp3": True},
+        }
+
+        self.in_vars["parameters"] = ["dt"]
+
+        self.out_vars = {
+            "z_edge": {"serialname": "zz_ze", "kend": namelist.npz, "mp3": True},
+            "z_terminal": {"serialname": "zz_zt", "kend": namelist.npz, "mp3": True},
+        }
+
+        self.stencil_factory = stencil_factory
+        self.grid_indexing = self.stencil_factory.grid_indexing
+        pconf = PhysicsConfig.from_namelist(namelist)
+        self.config = pconf.microphysics
+
+    def compute(self, inputs):
+        self.make_storage_data_input_vars(inputs)
+
+        inputs["tracer"] = "ice"
+
+        compute_func = ZeZt(
+            self.stencil_factory,
+            self.config,
+            timestep=inputs.pop("dt"),
+        )
+
+        compute_func(**inputs)
+
+        return self.slice_output(inputs)
