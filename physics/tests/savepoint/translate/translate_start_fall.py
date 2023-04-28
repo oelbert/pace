b1cb3a261d62725b0b8718b918e2b19fc5e4b74b
Oliver Elbert <oliver.elbert36@gmail.com>
2023-04-28 13:59:45 -0400

    testing a theory

--- physics/tests/savepoint/translate/translate_start_fall.py
+++ physics/tests/savepoint/translate/translate_start_fall.py
@@ -90,6 +90,7 @@ class TranslateStartFall(TranslatePhysicsFortranData2Py):
         self.stencil_factory = stencil_factory
         pconf = PhysicsConfig.from_namelist(namelist)
         self.config = pconf.microphysics
+        self.config.hydrostatic = True
 
     def compute(self, inputs):
         self.make_storage_data_input_vars(inputs)
