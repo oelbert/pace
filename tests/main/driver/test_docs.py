c1402d325a7d1968deaf46856ad0f555c5f28ed1
Oliver Elbert <oliver.elbert36@gmail.com>
2024-02-21 17:29:01 -0500

    trying to config for a merge into submodule

--- tests/main/driver/test_docs.py
+++
@@ -1,9 +0,0 @@
-import doctest
-
-import pace.driver.registry
-
-
-def test_registry_doc_examples():
-    result = doctest.testmod(pace.driver.registry)
-    assert result.attempted > 0, "No doctests found"
-    assert result.failed == 0, "doctests failed"
--- tests/savepoint/conftest.py
+++ tests/savepoint/conftest.py
@@ -1,65 +1,10 @@
-import os
+# This magical series of imports is to de-duplicate the conftest.py file
+# between the dycore and physics tests. We can avoid this if we refactor the tests
+# to all run from one directory
+import pace.stencils.testing.conftest
+from pace.stencils.testing.conftest import *  # noqa: F403,F401
 
-import pytest
-
-
-DIR = os.path.dirname(os.path.realpath(__file__))
-
-
-@pytest.fixture()
-def backend(pytestconfig):
-    backend = pytestconfig.getoption("backend")
-    return backend
+from . import translate
 
 
-@pytest.fixture()
-def data_path(pytestconfig):
-    data_path = pytestconfig.getoption("data_path")
-    return data_path
-
-
-@pytest.fixture()
-def threshold_path(pytestconfig):
-    threshold_path = pytestconfig.getoption("threshold_path")
-    if threshold_path is None:
-        threshold_path = os.path.join(DIR, "thresholds")
-    return threshold_path
-
-
-@pytest.fixture()
-def calibrate_thresholds(pytestconfig):
-    calibrate_thresholds = pytestconfig.getoption("calibrate_thresholds")
-    return calibrate_thresholds
-
-
-@pytest.fixture()
-def dperiodic(pytestconfig):
-    dperiodic = pytestconfig.getoption("dperiodic")
-    return dperiodic
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--backend", action="store", default="numpy", help="gt4py backend name"
-    )
-    parser.addoption(
-        "--data_path", action="store", default="./", help="location of reference data"
-    )
-    parser.addoption(
-        "--threshold_path",
-        action="store",
-        default=None,
-        help="directory containing comparison thresholds for tests",
-    )
-    parser.addoption(
-        "--calibrate_thresholds",
-        action="store_true",
-        default=False,
-        help="re-calibrate error thresholds for comparison to reference",
-    )
-    parser.addoption(
-        "--dperiodic",
-        action="store_true",
-        default=False,
-        help="configure tests for doubly-periodic domain",
-    )
+pace.stencils.testing.conftest.translate = translate  # type: ignore
