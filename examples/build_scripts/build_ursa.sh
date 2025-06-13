3f290f0200dc8f121d6eff8bb8ff2d47ecd48f6b
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-06-13 13:20:05 -0400

    adding recipe for gaea

---
+++ examples/build_scripts/build_ursa.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Example bash script to install Pace to run bare-metal on ursa cluster
+
+set -e -x
+
+# module load necessary system software
+module load gcc
+module load python/3.11
+module load openmpi
+module load netcdf-c
+module load cmake
+module load cuda
+
+
+python -m venv .vpace
+source .vpace/bin/activate
+
+pip install --upgrade pip setuptools wheel
+
+# If you want notebooks use these:
+# pip install jupyter
+# pip install ipyparallel
+
+# Install cupy for GPUs
+pip install cupy-cuda12x
+
+# Install pace
+pip install .[test]
