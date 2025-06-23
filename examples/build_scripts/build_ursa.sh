18418be1866d1105e954d46592feef6cceeccc36
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-06-23 13:44:51 -0400

    installs at least

--- examples/build_scripts/build_ursa.sh
+++ examples/build_scripts/build_ursa.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
 
-# Example bash script to install Pace to run bare-metal on ursa cluster
+# Example bash script to install Pace to run bare-metal on ursa cluster.
+# If you haven't already install miniconda for environment management (or use venv)
 
 set -e -x
 
 # module load necessary system software
-module load gcc
 module load python/3.11
 module load openmpi
 module load netcdf-c
 module load cmake
 module load cuda
 
+# DO NOT load the gcc module, openmpi was compiled against the default gcc version,
+# not the gcc in the module
 
-python -m venv .vpace
-source .vpace/bin/activate
+# Assuming you want to use conda:
+conda create -n pace python=3.11
+conda activate pace
+
+# # Else use a venv:
+# python -m venv .vpace
+# source .vpace/bin/activate
 
 pip install --upgrade pip setuptools wheel
 
@@ -28,5 +35,3 @@ pip install cupy-cuda12x
 # Install pace
 cd pace
 pip install .[test]
-
-#If this fails, be sure that /usr/include/ is in your PATH
